7a7c2be9f7e35325c6a5fb67663f38d5d428aba3
yongj.zhuang <yongj.zhuang@outlook.com>
2026-02-08 23:37:30 +0800

    Polishing

--- internal/repository/config_repository.go
+++ internal/repository/config_repository.go
@@ -1,16 +1,18 @@
 package repository
 
 import (
+	"github.com/curtisnewbie/miso/flow"
+	"github.com/curtisnewbie/miso/middleware/dbquery"
 	"github.com/curtisnewbie/nota/internal/domain"
 	"gorm.io/gorm"
 )
 
 // ConfigRepository defines the interface for config data operations
 type ConfigRepository interface {
-	Save(config *domain.Config) error
-	FindByName(name string) (*domain.Config, error)
-	FindAll() ([]*domain.Config, error)
-	Delete(name string) error
+	Save(rail flow.Rail, config *domain.Config) error
+	FindByName(rail flow.Rail, name string) (*domain.Config, error)
+	FindAll(rail flow.Rail) ([]*domain.Config, error)
+	Delete(rail flow.Rail, name string) error
 }
 
 // SQLiteConfigRepository implements ConfigRepository for SQLite
@@ -24,28 +26,50 @@ func NewSQLiteConfigRepository(db *gorm.DB) ConfigRepository {
 }
 
 // Save saves a config (create or update)
-func (r *SQLiteConfigRepository) Save(config *domain.Config) error {
-	return r.db.Save(config).Error
+func (r *SQLiteConfigRepository) Save(rail flow.Rail, config *domain.Config) error {
+	rail.Debugf("Saving config: %s", config.Name)
+	q := dbquery.NewQuery(rail, r.db).Table("config")
+	_, err := q.Create(config)
+	if err != nil {
+		rail.Errorf("Failed to save config %s: %v", config.Name, err)
+	} else {
+		rail.Infof("Successfully saved config: %s", config.Name)
+	}
+	return err
 }
 
 // FindByName finds a config by name
-func (r *SQLiteConfigRepository) FindByName(name string) (*domain.Config, error) {
+func (r *SQLiteConfigRepository) FindByName(rail flow.Rail, name string) (*domain.Config, error) {
+	rail.Debugf("Finding config by name: %s", name)
 	var config domain.Config
-	err := r.db.Where("name = ?", name).First(&config).Error
+	q := dbquery.NewQuery(rail, r.db).Table("config").Where("name = ?", name)
+	_, err := q.Scan(&config)
 	if err != nil {
+		rail.Warnf("Config not found: %s", name)
 		return nil, err
 	}
 	return &config, nil
 }
 
 // FindAll finds all configs
-func (r *SQLiteConfigRepository) FindAll() ([]*domain.Config, error) {
+func (r *SQLiteConfigRepository) FindAll(rail flow.Rail) ([]*domain.Config, error) {
+	rail.Debugf("Finding all configs")
 	var configs []*domain.Config
-	err := r.db.Find(&configs).Error
+	q := dbquery.NewQuery(rail, r.db).Table("config")
+	_, err := q.Scan(&configs)
+	rail.Debugf("Found %d configs", len(configs))
 	return configs, err
 }
 
 // Delete deletes a config by name
-func (r *SQLiteConfigRepository) Delete(name string) error {
-	return r.db.Where("name = ?", name).Delete(&domain.Config{}).Error
+func (r *SQLiteConfigRepository) Delete(rail flow.Rail, name string) error {
+	rail.Infof("Deleting config: %s", name)
+	q := dbquery.NewQuery(rail, r.db).Table("config").Where("name = ?", name)
+	_, err := q.Delete()
+	if err != nil {
+		rail.Errorf("Failed to delete config %s: %v", name, err)
+	} else {
+		rail.Infof("Successfully deleted config: %s", name)
+	}
+	return err
 }
